fbcab0af0ade736d2e40b0b1b05ec32a395b1598
Seyit Alaşhan <seyitalashan123@gmail.com>
2024-05-20 00:52:47 +0300

    1

--- Proje4/Assets/Scripts/Shot.cs
+++ Proje4/Assets/Scripts/Shot.cs
@@ -6,23 +6,34 @@ public class Shot : MonoBehaviour
 {
     RaycastHit hit;
     public AudioSource sound1, sound2;
+    public GameObject bulletObject;
+    public Transform bulletPoint;
     void Start()
     {
         Cursor.visible = false;
     }
     void Update()
     {
+        Cursor.visible = false;
         if (Input.GetKeyDown(KeyCode.Space))
         {
             sound1.Play();
+            Fire();
             if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, Mathf.Infinity))  
             {
                 if (hit.collider.gameObject.tag == "enemy")
                 {
-                    sound2.Play();
+                    sound2.Play();  
                     Destroy(hit.collider.gameObject);
                 }
             }
         }
+    }
+    public void Fire()
+    {
+        GameObject bullet = Instantiate(bulletObject, bulletPoint.position, bulletPoint.rotation) as GameObject;
+        Rigidbody bulletRb = bullet.GetComponent<Rigidbody>();
+        bulletRb.velocity = bulletPoint.forward * 1000f;
+        Destroy(bullet.gameObject, 0.2f);
     }
 }
